258980e590dd641a6d52b5642cf97fe816f65714
halo-dy <dongye649@gmail.com>
2026-07-29 09:41:43 +0800

    feat: initialize platform service

---
+++ services/platform-service/src/main/java/com/visualdefect/platform_service/PlatformServiceApplication.java
@@ -0,0 +1,13 @@
+package com.visualdefect.platform_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformServiceApplication.class, args);
+	}
+
+}
